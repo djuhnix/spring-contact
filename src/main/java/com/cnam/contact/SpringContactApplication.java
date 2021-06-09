132bc8c25a66e74f9f81eda8a02c21f273c41a5a
Germain OLEA <djuhngleen@gmail.com>
2021-06-09 15:00:46 +0200

    init

---
+++ src/main/java/com/cnam/contact/SpringContactApplication.java
@@ -0,0 +1,13 @@
+package com.cnam.contact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringContactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringContactApplication.class, args);
+    }
+
+}
